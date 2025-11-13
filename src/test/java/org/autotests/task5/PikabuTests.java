bbb806b91b965da5eec2f32e9b6230346f5db9a4
lvlde <li.vlade@gmail.com>
2025-11-13 21:54:32 +0300

    task 4 pikabu

--- src/test/java/org/autotests/task5/PikabuTests.java
+++ src/test/java/org/autotests/task5/PikabuTests.java
@@ -8,79 +8,68 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
 
 @DisplayName("Pikabu Tests")
 public class PikabuTests {
 
-    String url = "https://pikabu.ru/";
-    String expectedTitle = "Горячее – самые интересные и обсуждаемые посты | Пикабу";
-    String headerCloserButtonSelector = "data-testid=\"suggest:header:closer\"";
-    //<button class="eBPk20VGZ" data-t="suggest:header:closer" ><svg class="eoxtTP+dB" width="24" height="24" viewBox="0 0 24 24" fill="#262633" xmlns="http://www.w3.org/2000/svg"><path d="M19.2071 6.20711C19.5976 5.81658 19.5976 5.18342 19.2071 4.79289C18.8166 4.40237 18.1834 4.40237 17.7929 4.79289L12 10.5858L6.20711 4.79289C5.81658 4.40237 5.18342 4.40237 4.79289 4.79289C4.40237 5.18342 4.40237 5.81658 4.79289 6.20711L10.5858 12L4.79289 17.7929C4.40237 18.1834 4.40237 18.8166 4.79289 19.2071C5.18342 19.5976 5.81658 19.5976 6.20711 19.2071L12 13.4142L17.7929 19.2071C18.1834 19.5976 18.8166 19.5976 19.2071 19.2071C19.5976 18.8166 19.5976 18.1834 19.2071 17.7929L13.4142 12L19.2071 6.20711Z"></path></svg></button>
-    String enterButtonSelector = "[class=\"pkb-normal-btn\"]";
-    String authorizationModalSelector = "[class=\"auth-modal\"]";
-    String loginFieldSelector = "[class=\"auth-modal\"] [name=\"username\"]";
-    String passwordFieldSelector = "[class=\"auth-modal\"] [name=\"password\"]";
-    String submitButtonSelector = "[class=\"auth-modal\"] [class=\"button__title\"]";
-    String errorMessageSelector = "[class=\"popup__content\"] [class=\"auth__error auth__error_top\"]";
-    String expectedErrorText = "Ошибка. Вы ввели неверные данные авторизации";
+    private static final String URL = "https://pikabu.ru/";
+    private static final String EXPECTED_TITLE = "Горячее – самые интересные и обсуждаемые посты | Пикабу";
+    private static final String ENTER_BUTTON_SELECTOR = "[class=\"pkb-normal-btn header-right-menu__login-button\"]";
+    private static final String AUTHORIZATION_MODAL_SELECTOR = "[class=\"auth-modal\"]";
+    private static final String LOGIN_FIELD_SELECTOR = "[class=\"auth-modal\"] [name=\"username\"]";
+    private static final String PASSWORD_FIELD_SELECTOR = "[class=\"auth-modal\"] [name=\"password\"]";
+    private static final String SUBMIT_BUTTON_SELECTOR = "[class=\"auth-modal\"] [class=\"button__title\"]";
+    private static final String ERROR_MESSAGE_SELECTOR =
+            "[class=\"popup__content\"] [class=\"auth__error auth__error_top\"]";
+    private static final String LOGIN_KEYS_TO_SEND = "Qwerty";
+    private static final String PASSWORD_KEYS_TO_SEND = "Qwerty";
+    private static final String EXPECTED_ERROR_TEXT = "Ошибка. Вы ввели неверные данные авторизации";
 
-    WebDriver driver;
+    private WebDriver driver;
+    private WebDriverWait wait;
 
     @BeforeEach
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "C:/Users/ayyldyrym/Downloads/chromedriver-win64/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver",
+                "C:/Users/ayyldyrym/Downloads/chromedriver-win64/chromedriver.exe");
+
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--incognito");
         options.addArguments("--disable-blink-features=AutomationControlled");
+
         driver = new ChromeDriver(options);
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get(url);
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(100));
+        driver.manage().window().maximize();
+
+        driver.get(URL);
+
+        wait = new WebDriverWait(driver,Duration.ofSeconds(30));
     }
 
     @Test
     public void pikabuTest() {
+        Assertions.assertEquals(URL, driver.getCurrentUrl());
+        Assertions.assertEquals(EXPECTED_TITLE, driver.getTitle());
 
+        driver.findElement(By.cssSelector(ENTER_BUTTON_SELECTOR)).click();
 
-        Assertions.assertEquals(url, driver.getCurrentUrl());
-        Assertions.assertEquals(expectedTitle, driver.getTitle());
+        Assertions.assertTrue(driver.findElement(By.cssSelector(AUTHORIZATION_MODAL_SELECTOR)).isDisplayed());
+        Assertions.assertTrue(driver.findElement(By.cssSelector(LOGIN_FIELD_SELECTOR)).isDisplayed());
+        Assertions.assertTrue(driver.findElement(By.cssSelector(PASSWORD_FIELD_SELECTOR)).isDisplayed());
+        Assertions.assertTrue(driver.findElement(By.cssSelector(SUBMIT_BUTTON_SELECTOR)).isDisplayed());
 
-        //WebElement headerCloserButton = new WebDriverWait(driver, Duration.ofSeconds(90))
-        //        .until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(headerCloserButtonSelector)));
-        //headerCloserButton.click();
-        //driver.findElement(By.cssSelector(headerCloserButton)).click();
+        driver.findElement(By.cssSelector(LOGIN_FIELD_SELECTOR)).click();
+        driver.findElement(By.cssSelector(LOGIN_FIELD_SELECTOR)).sendKeys(LOGIN_KEYS_TO_SEND);
+        driver.findElement(By.cssSelector(LOGIN_FIELD_SELECTOR)).sendKeys(Keys.ENTER);
 
-        //TODO Кликнуть на кнопку «Войти».
-        //Assertions.assertTrue(driver.findElement(By.cssSelector(enterButtonSelector)).isDisplayed());
-        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", enterButtonSelector);
+        driver.findElement(By.cssSelector(PASSWORD_FIELD_SELECTOR)).click();
+        driver.findElement(By.cssSelector(PASSWORD_FIELD_SELECTOR)).sendKeys(PASSWORD_KEYS_TO_SEND);
+        driver.findElement(By.cssSelector(SUBMIT_BUTTON_SELECTOR)).click();
 
-        //WebElement enterButton = new WebDriverWait(driver, Duration.ofSeconds(10))
-         //       .until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(enterButtonSelector)));
-        //enterButton.click();
-
-
-
-        //TODO Убедиться, что отображается модальное окно «Авторизация»
-        Assertions.assertTrue(driver.findElement(By.cssSelector(authorizationModalSelector)).isDisplayed());
-
-        //TODO Убедиться, что отображаются поля «Логин» и «Пароль»
-        Assertions.assertTrue(driver.findElement(By.cssSelector(loginFieldSelector)).isDisplayed());
-        Assertions.assertTrue(driver.findElement(By.cssSelector(passwordFieldSelector)).isDisplayed());
-        //TODO Убедиться, что отображается кнопка «Войти»
-        Assertions.assertTrue(driver.findElement(By.cssSelector(authorizationModalSelector)).isDisplayed());
-        //TODO Ввести в поля данные в формате логин/пароль – Qwerty/Qwerty и нажать «Войти».
-        driver.findElement(By.cssSelector(loginFieldSelector)).click();
-        driver.findElement(By.cssSelector(loginFieldSelector)).sendKeys("Qwerty");
-        driver.findElement(By.cssSelector(loginFieldSelector)).sendKeys(Keys.ENTER);
-
-        driver.findElement(By.cssSelector(passwordFieldSelector)).click();
-        driver.findElement(By.cssSelector(passwordFieldSelector)).sendKeys("Qwerty");
-        driver.findElement(By.cssSelector(submitButtonSelector)).click();
-
-        //TODO Убедиться, что появилось сообщение об ошибке, и его текст: «Ошибка. Вы ввели неверные данные авторизации».
-        Assertions.assertTrue(driver.findElement(By.cssSelector(errorMessageSelector)).isDisplayed());
-        Assertions.assertEquals(driver.findElement(By.cssSelector(errorMessageSelector)).getText(), expectedErrorText);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(ERROR_MESSAGE_SELECTOR)));
+        Assertions.assertTrue(driver.findElement(By.cssSelector(ERROR_MESSAGE_SELECTOR)).isDisplayed());
+        Assertions.assertEquals(EXPECTED_ERROR_TEXT,
+                driver.findElement(By.cssSelector(ERROR_MESSAGE_SELECTOR)).getText());
     }
 
     @AfterEach
